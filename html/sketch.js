f4fa69206475774767793dc7c38c34b7310881db
Vinicius Franca <vinilima07@gmail.com>
2019-01-19 15:49:05 -0200

    up css modif

--- html/sketch.js
+++ html/sketch.js
@@ -15,9 +15,9 @@ function setup() {
 }
 
 function draw() {
-
 }
 
+//exibi em bloco se nao ouver espaço para inline
 function block(){
   if(window.innerWidth < 700){
       document.getElementById("sec-menu-desktop").style.display = "none";
@@ -26,6 +26,7 @@ function block(){
   }
 }
 
+//exibi o menu responsivo
 function showMenu(){
   if(window.innerWidth < 700){
     var desk = document.getElementById("sec-menu-desktop");
@@ -47,37 +48,56 @@ function chamaTable(){
 
 /*INSERE NA TABLE AUTOMATICAMENTE*/
 function insereTable(tableName){
-  var table, value, data, row, celula, tam;
+  var table, value, data, row, celula, iframeClass;
 
   if(tableName == "tablePodcast"){
     data = dataPodCast;
+    iframeClass = "podcastTD";
     tam = dataPodCast.lenght;
   }else if(tableName == "tableEsport"){
     data = dataEsport;
+    iframeClass = "esportTD";
     tam = dataEsport.lenght;
   }else{
     data = dataGameplay;
+    iframeClass = "gameplayTD";
     tam = dataGameplay.lenght;
   }
-
   table = document.getElementById(tableName).getElementsByTagName('tbody')[0];
 
   for(i=19; i>=0 ; i--){
-    //pega o id da tabela passada como parametro
-    row = document.createElement("tr")    //cria uma linha vazia
-    celula = document.createElement("td");    //cria uma celula de dados
-    link = document.createElement("a");
+    row = document.createElement("tr")
+    celula = document.createElement("td");
+    link = document.createElement("button");
     link.setAttribute("href", data[i].link);
+    link.setAttribute("class", iframeClass);
     link.innerHTML = data[i].titulo;
+    link.style.width = "100%";
     celula.appendChild(link);
     row.appendChild(celula);
     table.appendChild(row);
+    link.addEventListener("click", function(){relationIframe(this)});
   }
 }
 
+//seta o tamanho das tabelas de acordo com o tamanho do video
 function resizeTable(){
   for(i = 0; i<3; i++){
     var tblH = document.getElementsByClassName("video-wrapper")[i].offsetHeight;
     document.getElementsByTagName("tbody")[i].style.height = tblH+"px";
+  }
+}
+
+//cria a relacao do iframe com o episodio escolhido
+function relationIframe(Obj){
+  if(Obj.getAttribute("class") == "podcastTD"){
+    video = document.getElementById("sec-podcast").getElementsByTagName("iframe")[0];
+    video.setAttribute("src", Obj.getAttribute("href"));
+  }else if(Obj.getAttribute("class")  == "esportTD"){
+    video = document.getElementById("sec-esport").getElementsByTagName("iframe")[0];
+    video.setAttribute("src", Obj.getAttribute("href"));
+  }else if(Obj.getAttribute("class")  == "gameplayTD"){
+    video = document.getElementById("sec-gameplays").getElementsByTagName("iframe")[0];
+    video.setAttribute("src", Obj.getAttribute("href"));
   }
 }
